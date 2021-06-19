b72d77cc71aa3a99d42229b08d46007bb66ec061
Hani Hussein <hani.hussein94@gmail.com>
2021-06-19 23:06:28 +0200

    create note model

---
+++ lib/model/note.dart
@@ -0,0 +1,31 @@
+import 'dart:typed_data';
+
+class Note {
+  int? id;
+  String? title;
+  String? description;
+  DateTime? date;
+  bool? status;
+  Uint8List? picture;
+
+  Note(this.id, this.title, this.description, this.date, this.status,
+      this.picture);
+
+  Note.fromMap(Map map) {
+    id = map[id];
+    title = map[title];
+    description = map[description];
+    date = map[date];
+    status = map[status];
+    picture = map[picture];
+  }
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "title": title,
+        "description": description,
+        "date": date,
+        "status": status,
+        "picture": picture,
+      };
+}
